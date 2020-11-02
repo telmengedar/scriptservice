5eb3ef68a34ec67649b2eb274e86d23de3c71081
Toni Gottschall <toni.gottschall@nightlycode.de>
2020-11-02 02:43:06 +0100

    added test for ordering of task list

---
+++ ScriptService.Tests/TaskServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using NightlyCode.AspNetCore.Services.Data;
+using NightlyCode.Database.Entities;
+using NUnit.Framework;
+using ScriptService.Dto.Tasks;
+using ScriptService.Services;
+
+namespace ScriptService.Tests {
+    
+    [TestFixture, Parallelizable]
+    public class TaskServiceTests {
+
+        [Test, Parallelizable]
+        public async Task MostRecentFirst() {
+            IEntityManager database = TestSetup.CreateMemoryDatabase();
+            DatabaseTaskService taskservice = new DatabaseTaskService(database);
+
+            for (int i = 0; i < 31; ++i) {
+                await taskservice.StoreTask(new WorkableTask() {
+                    Id = Guid.NewGuid(),
+                    Started = new DateTime(2020, 01, 1 + i),
+                    Finished = new DateTime(2020, 01, i + 1, 1, 0, 0)
+                });
+            }
+
+            Page<WorkableTask> tasks = await taskservice.ListTasks(new TaskFilter {
+                Count = 10,
+            });
+
+            Assert.AreEqual(10, tasks.Result.Length);
+            Assert.AreEqual(31, tasks.Result[0].Started.Day);
+        }
+    }
+}
